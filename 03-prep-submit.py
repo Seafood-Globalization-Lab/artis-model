fbb2f9c26b03c3b26abc83f23372c7b1e47aebd1
“rahulAgrBej” <rahul.agrawal.b@gmail.com>
2023-05-15 12:03:46 -0400

    editing prep zorro submit workflow file creation

--- 03-prep-submit.py
+++ 03-prep-submit.py
@@ -4,6 +4,11 @@ import re
 
 hs_versions = ['96', '02', '07', '12', '17']
 
+# read in zorro set up file
+f = open('00-zorro-hpc-setup.R')
+zorro_setup_content = f.read()
+f.close()
+
 # read in original file
 f = open('02-artis-pipeline.R', 'r')
 artis_file_content = f.read()
@@ -15,13 +20,25 @@ if not os.path.isdir(submit_folder):
 
 for hs_version in hs_versions:
   
-  new_artis_content = re.sub('hs_version_run <- \"17\"', 'hs_version_run <- \"' + hs_version + '\"', artis_file_content)
+  new_zorro_setup_content = re.sub('hs_version_run <- \"17\"', 'hs_version_run <- \"' + hs_version + '\"', zorro_setup_content)
+  new_artis_content = re.sub('# source\(\"00-zorro-hpc-setup.R\"\)', 'source("00-zorro-hpc-setup_HS' + hs_version +'.R")', artis_file_content)
+  new_artis_content = re.sub('source\(\"00-demo-setup.R\"\)', '# source\(\"00-demo-setup.R\"\)', new_artis_content)
   
-  # create new file name
+  # create new Zorro setup file
+  curr_setup_filename = '00-zorro-setup_HS' + hs_version + '.R'
+  curr_setup_fp = os.path.join(submit_folder, curr_setup_filename)
+  curr_setup_f = open(curr_setup_fp, 'w')
+  curr_setup_f.write(new_zorro_setup_content)
+  curr_setup_f.close()
+  
+  print('Done writing ' + curr_setup_filename)
+  
+  # create new ARTIS pipeline file
   curr_filename = 'ARTIS_pipeline_HS' + hs_version + '.R'
   curr_fp = os.path.join(submit_folder, curr_filename)
   curr_f = open(curr_fp, 'w')
   curr_f.write(new_artis_content)
+  curr_f.close()
   
   print('Done writing ' + curr_filename)
   
