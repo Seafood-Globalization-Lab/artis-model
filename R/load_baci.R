21f367bab3494848f68d7afae9a707f13914d08b
theamarks <theamarks@gmail.com>
2025-01-28 16:03:08 -0800

    formatting for clarity. Changed raw baci column names in joining, for latest baci release 202401b

--- R/load_baci.R
+++ R/load_baci.R
@@ -6,14 +6,31 @@ load_baci <- function(baci_data, hs_codes, baci_country_codes){
     rename(hs6 = k) %>% 
     # Filter to hs codes included in analysis
     filter(hs6 %in% hs_codes) %>% 
-    # Match Comtrade country codes to ISO3c codes
-    full_join(baci_country_codes, by = c("i" = "country_code")) %>%
-    select(t, exporter_iso3c = iso_3digit_alpha, exporter_iso3n = i,
-           exporter_country = country_name_abbreviation, j, hs6, v, q) %>%
-    full_join(baci_country_codes, by = c("j" = "country_code")) %>%
-    select(t, hs6, exporter_iso3c, exporter_iso3n, exporter_country,
-           importer_iso3c = iso_3digit_alpha, importer_iso3n = j,
-           importer_country = country_name_abbreviation, v, q) %>%
+    # Match Comtrade country codes to ISO3c codes - (i) exporter
+    full_join(baci_country_codes, 
+              by = c("i" = "country_code")) %>%
+    select(t, # year
+           exporter_iso3c = country_iso3, #AM - baci_country_codes$country_iso3 ? 
+           exporter_iso3n = i, #AM - still i (BACI country code/key)
+           exporter_country = country_name, #AM - baci_country_codes$country_name
+           j, # BACI country code/key
+           hs6, # HS product code
+           v, # value
+           q # quantity
+           ) %>%
+    # Match Comtrade country codes to ISO3c codes - (j) importer
+    full_join(baci_country_codes, 
+              by = c("j" = "country_code")) %>%
+    select(t, 
+           hs6, 
+           exporter_iso3c, 
+           exporter_iso3n, 
+           exporter_country,
+           importer_iso3c = country_iso3, #AM - baci_country_codes$country_iso3?
+           importer_iso3n = j, # BACI country code/key
+           importer_country = country_name, 
+           v, 
+           q) %>%
     filter(!is.na(t))
   
   # Add leading "0" back to those under length of 6
