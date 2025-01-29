f94628c7ba5d106bff422fac4d5bf5b417211920
theamarks <theamarks@gmail.com>
2025-01-29 14:43:01 -0800

    clarify comments in standardize_countries add function arguements explicity for clarity

--- R/standardize_countries.R
+++ R/standardize_countries.R
@@ -7,20 +7,22 @@ standardize_countries <- function(df,
   standard_df <- data.frame()
   
   if (data_source == "FAO") {
-    
-    standard_df <- standardize_prod(df,
-                                    "country_iso3_alpha",
-                                    "country_name_en") %>%
+    # standardize country names
+    standard_df <- standardize_prod(data = df, 
+                                    col_iso3 = "country_iso3_alpha", 
+                                    col_country_name = "country_name_en") %>%
+      # remove original FAO country names and rename
       select(-c(country_iso3_alpha, country_name_en)) %>%
       rename(country_iso3_alpha = artis_iso3c, 
              country_name_en = artis_country_name) %>%
       mutate(country_iso3_numeric = countrycode(country_iso3_alpha, "iso3c", "iso3n")) %>%
+      
       # group by all raw SAU data columns if all_sau_cols is TRUE
       { if (all_sau_cols == TRUE) {
         group_by(., country_iso3_alpha, SciName, CommonName, taxa_source, year, 
                  Species01, Genus01, Family01, Other01, habitat, prod_method, gear,
                  eez, sector, end_use)
-      # otherwise if all_sau_cols argument is empty or explicitly FALSE do this:
+        # otherwise if all_sau_cols argument is empty or explicitly FALSE do this:
       } else {
         group_by(., country_iso3_alpha, SciName, CommonName, taxa_source, year, 
                  Species01, Genus01, Family01, Other01, habitat, prod_method)
@@ -34,15 +36,22 @@ standardize_countries <- function(df,
     
   } else if (data_source == "BACI") {
     
-    standard_df <- standardize_baci(df, "exporter_iso3c", "exporter_country") %>%
+      # standardize country names - exporter
+    standard_df <- standardize_baci(data = df, 
+                                    col_iso3 = "exporter_iso3c", 
+                                    col_country_name = "exporter_country") %>%
+      # remove original BACI country names and rename
       select(-c(exporter_iso3c, exporter_country)) %>%
       rename(exporter_iso3c = artis_iso3,
              exporter_country = artis_country_name) %>%
-      standardize_baci("importer_iso3c", "importer_country") %>%
+      # standardize country names - importer
+      standardize_baci(col_iso3 = "importer_iso3c", 
+                       col_country_name = "importer_country") %>%
+      # remove original BACI country names and rename
       select(-c(importer_iso3c, importer_country)) %>%
       rename(importer_iso3c = artis_iso3,
              importer_country = artis_country_name) %>%
-      # Removing country names so that territories do not get confused with their sovereign nations
+      # aggregate total_q and total_v to account for territories converted to sovereign states
       group_by(exporter_iso3c, importer_iso3c, hs6, year, hs_version) %>%
       summarize(total_q = sum(total_q, na.rm = TRUE),
                 total_v = sum(total_v, na.rm = TRUE)) %>%
