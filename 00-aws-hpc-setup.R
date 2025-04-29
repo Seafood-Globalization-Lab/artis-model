2f4cd2a744fe2f778980ae4e4f9f540a12ca2649
theamarks <theamarks@gmail.com>
2025-04-29 09:33:10 -0700

    clean up 00- configure scripts

--- 00-aws-hpc-setup.R
+++ 00-aws-hpc-setup.R
@@ -1,5 +1,7 @@
+# Model configuration parameters for running ARTIS pipeline on AWS infrastructure
 
-# libraries
+# sourced from 02-artis-pipeline
+# R packages required ------------------------------------------------------
 library(data.table)
 library(magrittr)
 library(Matrix)
@@ -12,6 +14,7 @@ library(aws.s3)
 # loading in ARTIS R package
 library(artis)
 
+# configure environment ---------------------------------------------------
 # Create and download ARTIS R package
 setwd('/usr/src/ARTIS')
 
@@ -20,18 +23,29 @@ readRenviron(".Renviron")
 # ONLY UNCOMMENT IF YOU ARE RUNNING DOCKER ON A NEW MAC CHIP
 use_virtualenv("/usr/src/ARTIS/venv", required = TRUE)
 
+# AWS variables -----------------------------------------------------------
 # Download files to create ARTIS R package from AWS S3
 artis_bucket <- "s3://artis-s3-bucket/"
 artis_bucket_region <- "us-east-1"
 
-# HS version being run
+# ARTIS model parameters ----------------------------------------------
+# HS version to run in this pipeline script. 
+# artis-hpc/create-pipeline-versions.sh script automatically changes this line 
+# based on $HS_VERSIONS values set to submit multiple jobs to AWS Batch - 
+# Do not manually change for AWS.
 hs_version_run <- "07"
 
+# set specific years to run in number vector, leave empty to run all years
 test_years <- c()
 
+# set model estimate - "min", "midpoint", "max" - default is "midpoint"
+estimate_data_type <- "midpoint"
+
+# directories -------------------------------------------------------------
 datadir <- "model_inputs"
 outdir <- "outputs"
-
-estimate_data_type <- "midpoint"
 outdir_snet <- file.path(outdir, "snet")
+
+
+
 
--- 00-local-machine-setup.R
+++ 00-local-machine-setup.R
@@ -1,6 +1,8 @@
+# Model configuration parameters for running ARTIS pipeline on a local machine
 
-# Set up for running ARTIS pipeline on a local machine
-# R packages needed
+# Sourced from scripts 01-clean-model-inputs and 02-artis-pipeline
+
+# R packages required ------------------------------------------------------
 library(artis)
 library(data.table)
 library(magrittr)
@@ -16,7 +18,7 @@ library(countrycode)
 # Getting the start date to identify files generated in this ARTIS run
 start_date <- as.character(Sys.Date())
 
-# Set directory paths
+# Directory paths --------------------------------------------------
 datadir_raw <- "~/Documents/UW-SAFS/ARTIS/data/model_inputs_raw"
 # Directory for inputs to create the ARTIS database
 datadir <- "AM_local/model_inputs"
@@ -29,19 +31,27 @@ outdir_sql <- file.path(outdir, "sql_database")
 baci_version <- "202201"
 tradedatadir <- paste("baci_raw/baci_", baci_version, sep = "")
 
-# set years to run - 02-artis-pipeline 
-# empty if all years [c()]
+# ARTIS model parameters 02-artis-pipeline ------------------------------------
+# set years to run - empty if all years [c()]
 test_years <- c(2006) 
+
+# set model estimate - "min", "midpoint", "max" - default is "midpoint"
+estimate_data_type <- "midpoint"
 
 # Set production data type variable ["SAU"] or ["FAO"] - 02-artis-pipeline
 prod_data_type <- "SAU"
 
+# hs_version_run is set in 02-artis-pipeline because of current `artis-hpc` setup
+
+# data cleaning options 01-clean-model-inputs --------------------------------
 # Model Mode for 01-clean-model-inputs - TRUE for SAU; FALSE for FAO
 running_sau <- TRUE
 
 ## Set TRUE if new SeaLifeBase/FishBase data collection needed for 01-clean-model-inputs:
 need_new_fb_slb <- FALSE
 
+
+# python environment - 02-artis-pipeline ----------------------------------
 # Linking python environment set up during installation for use in the pipeline
 #python_path <- file.path(getwd(), "venv", "bin", "python3")
 #use_python(python_path, required = TRUE)
@@ -49,11 +59,8 @@ need_new_fb_slb <- FALSE
 python_path <- file.path(getwd(), "venv")
 use_virtualenv(python_path, required = TRUE)
 
-# ARTIS folder structure needs to be created differently when running on the
-# High Performance Computing sytem
-hpc_run <- FALSE
-
-# need explicity empty AWS values when running locally
+# empty AWS values --------------------------------------------------------
+# need explicit empty AWS values when running locally - do not change
 artis_bucket <- ""
 artis_bucket_region <- ""
 
@@ -61,6 +68,7 @@ artis_bucket_region <- ""
 # Not sure if working AM 2025-04-28
 # Will determine if the ARTIS pipeline functions are run with the demo variables
 # or to run functions to create the ARTIS database for all years and HS versions
+# TRUE or FALSE explicitly required for several conditionals within model functions
 demo_run <- FALSE
 
 # If running a test environment with specific codes scinames this variable should be true else false
