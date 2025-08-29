9ad824dcf4e20ea94ced58964ca88c0638409564
theamarks <theamarks@gmail.com>
2025-08-29 11:45:37 -0700

    add more verbose and formatted warning to create_reweight_X_long

--- R/create_reweight_X_long.R
+++ R/create_reweight_X_long.R
@@ -17,10 +17,17 @@ create_reweight_X_long <- function(country_est, V1, V2) {
     mutate(difference = 1 - reweighted_X) %>%
     filter(abs(difference) > 1e-9)
   
-  if (nrow(check_reweight_X_long)) {
-    warning("Reweighted X long proportions DO NOT sum to 1.")
-    return(NULL)
-  }
+if (nrow(check_reweight_X_long)) {
+  cli_warn(c(
+    "!" = "Reweighted X long proportions DO NOT sum to 1.",
+    "Problematic iso3c–hs6 pairs:",
+    setNames(
+      glue("{check_reweight_X_long$iso3c} – {check_reweight_X_long$hs6}"),
+      rep("*", nrow(check_reweight_X_long))
+    )
+  ))
+  return(NULL)
+}
   
   return(reweight_X_long)
 }
