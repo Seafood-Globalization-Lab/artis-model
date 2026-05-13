5a688e6c9e52eb80b68fe7f8e8276549dc23a661
Connor <quir1869@uw.edu>
2026-05-13 16:09:00 -0700

    Begin testing of new standardize_countries function

--- inst/test_standardize_countries_2026_05_06.R
+++ inst/test_standardize_countries_2026_05_06.R
@@ -11,9 +11,9 @@
 }
 
 # Connor's path
-#path_local <- "QA"
+path_local <- "QA"
 # Althea's path
-path_local <- "AM_local"
+# path_local <- "AM_local"
 
 # read in raw combo files as controls
 raw_fao <- read_csv(glue("./{path_local}/dev-standardize-countries/1.2_fao_country_combos.csv"))
@@ -26,6 +26,60 @@ std_baci <- read_csv(glue("./{path_local}/dev-standardize-countries/1.2_baci_std
 
 # Validation Outline / Plan
 
-## Run each raw data file through artis::standardize_countries() 
+## Run validation check differences between raw and standardized datasets
 
-## compare 
+### FAO
+nrow(raw_fao) # larger amount of rows in raw than in standardized
+nrow(std_fao)
+
+### Quite a bit of filtering that happens in the FAO data from raw to standardized, filter by year
+unique(raw_fao$country_iso3_alpha) %>% length() # 235 countries & territories
+unique(std_fao$country_iso3_alpha) %>% length() # 193 sovereign countries
+
+### SAU
+nrow(raw_sau) # larger amount of rows in raw than in standardized
+nrow(std_sau)
+
+unique(raw_sau$country_name_en) %>% length() # 198 countries & territories
+unique(std_sau$country_name_en) %>% length() # 153 sovereign countries
+
+### BACI
+nrow(raw_baci) # larger amount of rows in raw than in standardized
+nrow(std_baci)
+
+unique(raw_baci$country_iso3_alpha) %>% length() # 235 countries & territories
+unique(std_baci$country_iso3_alpha) %>% length() # 193 sovereign countries
+
+## Run each raw data file through artis::standardize_countries()
+
+### Correct by iso3
+test_std_fao <- artis::standardize_countries(data = raw_fao,
+                             country_id_type = "iso3c",
+                             country_col_name = "country_iso3_alpha",
+                             year_col_name = "year")
+
+### Correct by country name
+test_std_fao <- artis::standardize_countries(data = raw_fao,
+                                             country_id_type = "name_en",
+                                             country_col_name = "country_name_en",
+                                             year_col_name = "year")
+
+### Compare new standardized data to input data
+x <- test_std_fao %>% distinct(artis_iso3c, year) %>%
+  rename(country_iso3_alpha = artis_iso3c) # 4740 rows
+y <- std_fao %>% select(country_iso3_alpha, year) # 4752 rows
+
+### How our corrections currently differ: We include ZA1
+### Whats old corrections have that our corrections don't have: NEI & SDN
+setdiff(na.omit(x), y) 
+setdiff(y, x) %>% View()
+
+## Compare outputs of the new standardized data to the old standardized data
+
+### Extract any records that don't match, and go from there
+
+### There's the same number of records that are in the raw and standardized datasets
+
+
+
+
